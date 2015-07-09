5c68cd97d92effc2071217e774ffb608ba9f6abb
Shawn Sandy <shawnsandy04@gmail.com>
2015-07-08 21:38:17 -0400

    Release package

--- component/vendor.js
+++ component/vendor.js
@@ -1,13 +1,1 @@
-webpackJsonp([1],[
-/* 0 */
-/*!********************!*\
-  !*** multi vendor ***!
-  \********************/
-/***/ function(module, exports, __webpack_require__) {
-
-	module.exports = __webpack_require__(/*! react */1);
-
-
-/***/ }
-]);
-//# sourceMappingURL=vendor.js.map
+webpackJsonp([2],[function(e,t,n){e.exports=n(58)}]);
